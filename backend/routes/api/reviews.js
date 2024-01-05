802969d7eac999e28e583617cdbac8e9b0f9b62e
chrisfealy <chrismfealy@gmail.com>
2024-01-05 00:13:40 -0600

    refactored endpoints in all api routes

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { requireAuth } = require('../../utils/auth')
 const { Review, ReviewImage, Spot } = require('../../db/models')
 const router = express.Router()
+const { check } = require('express-validator')
 const { handleValidationErrors } = require('../../utils/validation')
 
 const validateReviews = [
