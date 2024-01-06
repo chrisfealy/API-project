9688d52ae190b4280024066403f7c9b6a7269f71
chrisfealy <chrismfealy@gmail.com>
2024-01-05 18:04:29 -0600

    fixed users.js

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -2,9 +2,10 @@ const express = require('express')
 const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
-const router = express.Router();
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
 
 const validateSignup = [
   check('email')
@@ -13,8 +14,8 @@ const validateSignup = [
     .withMessage('Please provide a valid email.'),
   check('username')
     .exists({ checkFalsy: true })
-    .isLength({ min: 3 })
-    .withMessage('Please provide a username with at least 3 characters.'),
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
   check('username')
     .not()
     .isEmail()
@@ -23,23 +24,31 @@ const validateSignup = [
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
     .withMessage('Password must be 6 characters or more.'),
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 2 })
+    .withMessage('First Name should be at least 2 characters long.'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 2 })
+    .withMessage('Last Name should be at least 2 characters long.'),
   handleValidationErrors
 ];
 
 // Sign up
-router.post(
-  '/',
-  validateSignup,
-  async (req, res) => {
-    const { email, password, username } = req.body;
-    const hashedPassword = bcrypt.hashSync(password);
-    const user = await User.create({ email, username, hashedPassword });
 
-    const safeUser = { id: user.id };
-    if(user.firstName) safeUser.firstName = user.firstName;
-    if(user.lastName) safeUser.lastName = user.lastName;
-    safeUser.email = user.email;
-    safeUser.username = user.username
+router.post('/', validateSignup, async (req, res) => {
+    const { firstName, lastName, email, password, username } = req.body;
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({ firstName, lastName, email, username, hashedPassword });
+
+    const safeUser = {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      username: user.username
+    };
 
     await setTokenCookie(res, safeUser);
 
