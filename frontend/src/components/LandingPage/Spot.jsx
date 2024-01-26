cf4f7df367b0a95acb51f1f4497bab9130c062cc
chrisfealy <chrismfealy@gmail.com>
2024-01-26 16:19:47 -0600

    landing page styling

--- frontend/src/components/LandingPage/Spot.jsx
+++ frontend/src/components/LandingPage/Spot.jsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom';
 
 function Spot({ spot }) {
     return (
-        <div className="spot-card">
+        <div className="spot-card" title={spot.name}>
             <Link to={`/spots/${spot.id}`}>
-            <img src={spot.previewImage} alt={spot.name} className="spot-card-image" />
-            <div className="spot-info">
-                <p>{spot.city}, {spot.state}</p>
-                <p><i className="fa-solid fa-star"/>{(spot.avgRating && Number(spot.avgRating).toFixed(1)) || `New`}</p>
-            </div>
-            <p>${spot.price} night</p>
+                <img src={spot.previewImage} alt={spot.name} className="spot-card-image" />
+                <div className="spot-info">
+                    <p>{spot.city}, {spot.state}</p>
+                    <p><i className="fa-solid fa-star" />{(spot.avgRating && Number(spot.avgRating).toFixed(1)) || `New`}</p>
+                </div>
+                <p>${spot.price} night</p>
             </Link>
         </div>
     )
