8b8fb8e75f40cc6b002aa4f080b0e3d7e294af5a
chrisfealy <chrismfealy@gmail.com>
2024-03-15 11:19:10 -0500

    updated styling for nav + login/signup

--- frontend/src/components/Navigation/Navigation-bonus.jsx
+++ frontend/src/components/Navigation/Navigation-bonus.jsx
@@ -8,28 +8,31 @@ function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
   return (
-    <div className='nav-container'>
-      <div className='nav-left'>
-        <NavLink to="/">
-          <img src={logo} alt="dairbnb" className='nav-logo' />
-        </NavLink>
-        <NavLink to="/">
-          <h2>dairbnb</h2>
-        </NavLink>
+    <>
+      <div className='nav-container'>
+        <div className='nav-left'>
+          <NavLink to="/">
+            <img src={logo} alt="dairbnb" className='nav-logo' />
+          </NavLink>
+          <NavLink to="/">
+            <h2>dairbnb</h2>
+          </NavLink>
+        </div>
+        <div className='nav-right'>
+          {sessionUser && (
+            <div className='nav-new-spot'>
+              <NavLink to="/spots/new">Create a New Spot</NavLink>
+            </div>
+          )}
+          {isLoaded && (
+            <div>
+              <ProfileButton user={sessionUser} />
+            </div>
+          )}
+        </div>
       </div>
-      <div className='nav-right'>
-        {sessionUser && (
-          <div className='nav-new-spot'>
-            <NavLink to="/spots/new">Create a New Spot</NavLink>
-          </div>
-        )}
-        {isLoaded && (
-          <div>
-            <ProfileButton user={sessionUser} />
-          </div>
-        )}
-      </div>
-    </div>
+      <hr className="nav-line" />
+    </>
   );
 }
 
